3cc469fd31da6b9a5a79086c73f8d3a84899581c
MoiraDust <moiradust@foxmail.com>
2022-07-25 21:53:57 +1000

    14

--- 14.longest-common-prefix.js
+++ 14.longest-common-prefix.js
@@ -9,8 +9,32 @@
  * @param {string[]} strs
  * @return {string}
  */
-var longestCommonPrefix = function(strs) {
-    
+var longestCommonPrefix = function (strs) {
+  var prefix = [];
+  if (strs.length == 0) {
+    return prefix;
+  }
+  //   找到最短的那个字符串
+  let minLen = strs[0].length;
+  for (let i = 1; i < strs.length; i++) {
+    if (strs[i].length <= minLen) {
+      minLen = strs[i].length;
+    }
+  }
+  //   console.log("minLen", minLen);
+  var index = 0;
+  outer: while (index < minLen) {
+    let char = strs[0][index];
+    console.log(index + "char: " + char);
+    for (let j = 1; j < strs.length; j++) {
+      if (strs[j][index] !== char) {
+        break outer;
+      }
+    }
+    prefix.push(char);
+    index++;
+  }
+  //   convert array to string
+  return prefix.join("");
 };
 // @lc code=end
-
